bc98e732b3e358976aba62384c5a982ce232cb35
MContour <m-contour@qq.com>
2022-09-23 15:45:04 +0800

    update

--- Level-3/03_serialport/mainwindow.h
+++ Level-3/03_serialport/mainwindow.h
@@ -21,5 +21,41 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private:
+    QSerialPort *serialPort;
+    QTextBrowser *textBrowser;  // received data display module
+    QTextEdit *textEdit;        // data sender
+    QPushButton *pushButton[2];
+    QComboBox *comboBox[5];     // selection box
+    QLabel *label[5];
+    QVBoxLayout *vboxLayout;
+    QGridLayout *gridLayout;
+    QWidget *mainWidget;
+    QWidget *funcWidget;
+
+    /* 布局初始化 */
+    void layoutInit();
+
+    /* 扫描系统可用串口 */
+    void scanSerialPort();
+
+    /* 波特率项初始化 */
+    void baudRateItemInit();
+
+    /* 数据位项初始化 */
+    void dataBitsItemInit();
+
+    /* 检验位项初始化 */
+    void parityItemInit();
+
+    /* 停止位项初始化 */
+    void stopBitsItemInit();
+
+private slots:
+    void sendPushButtonClicked();
+    void openSerialPortPushButtonClicked();
+    void serialPortReadyRead();
+
 };
 #endif // MAINWINDOW_H
